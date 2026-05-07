59ea129e07f5e3ae737d093b2623c512688e6bb7
moshoubash <ma016188@gmail.com>
2026-05-07 10:54:09 +0300

    feat: enable public post fetching and clean up unused logic in getPostsForAuthenticatedUsers

--- api/posts/route.ts
+++ api/posts/route.ts
@@ -6,24 +6,25 @@ if (!BACKEND_API_URL) {
 
 import { getStorageItem } from "@/lib/storage";
 
-// export async function getPosts() {
-//     const res = await fetch(`${BACKEND_API_URL}/posts`, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json',
-//         },
-//     });
-//     const posts = await res.json();
-//     return posts;
-// }
+export async function getPosts() {
+    const res = await fetch(`${BACKEND_API_URL}/posts`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    });
+    const posts = await res.json();
+    return posts;
+}
 
 export async function getPostsForAuthenticatedUsers() {
     const token = await getStorageItem('token');
 
-    // if(!token) {
-    //     return getPosts();
-    // }
+    if(!token) {
+        return getPosts();
+    }
+
     const res = await fetch(`${BACKEND_API_URL}/posts/get-posts-for-authenticated-users`, {
         method: 'GET',
         cache: 'no-store',
@@ -35,8 +36,6 @@ export async function getPostsForAuthenticatedUsers() {
     });
 
     const posts = await res.json();
-
-    console.log(posts);
 
     return posts;
 }
